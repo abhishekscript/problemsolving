2dd25b0090314e75e315c2a26430a0f69adaa82c
Abhishek Rana <abhishekscript@gmail.com>
2023-01-02 21:37:50 +0530

    LRU cache

--- lru.py
+++ lru.py
@@ -1,40 +1,53 @@
-
-from collections import OrderedDict
-class Cache:
-	def __init__(self, size):
-		self.hashMap = OrderedDict({})
-		self.size 	 = size
-		self.ptr 	 = -1
-
-
-	def add(self, key, value):
-		
-		self.hashMap[ key ] = value
-		self.ptr +=1
-		if self.ptr == self.size:
-			self.hashMap.pop( list(self.hashMap.keys())[0] )
-			self.ptr-=1
-
-	def get(self, key):
-		if self.hashMap.get(key) == None:
-			raise ValueError
-
-		self.hashMap.move_to_end(key)
-		return self.hashMap[key]
+class Node:
+    def __init__(self, key, value, prev=None, next=None) -> None:
+        self.key = key
+        self.vale = value
+        self.prev = prev
+        self.next = next
 
 
-	def __repr__(self):
-		return f'{self.hashMap}'
+class LRUCache:
+    def __init__(self, capacity) -> None:
+        self.capactiy = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-lru = Cache( 5 )
-lru.add(1,2)
-lru.add(2,"A")
-lru.add(3,"B")
-lru.add(8,"9")
-lru.add(4,"C")
-lru.get(1)
-lru.add(5,"D")
-lru.add(6,"E")
-lru.add(7,"F")
+    def get(self, key):
+        if key not in self.cache:
+            return -1
 
-print( lru )
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.value
+
+    def set(self, key, value):
+        if key in self.cache:
+            self._remove(self.cache[key])
+
+        node = Node(key, value)
+        self._add(node)
+        self.cache[key] = node
+        if len(self.cache) > self.capactiy:
+            oldest_node = self.head.next
+            self._remove(oldest_node)
+            del self.cache[oldest_node.key]
+
+    def _remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def _add(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        self.tail.prev = node
+        node.prev = prev
+        node.next = self.tail
+
+
+
